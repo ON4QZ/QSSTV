d493b1270406cde43871a0213c58bd15bca113d2
Joerg Wunsch <j@uriah.heep.sax.de>
2022-06-03 23:09:40 +0200

    Remove <malloc.h>
    It is not needed anyway; malloc() is sufficiently defined in
    <stdlib.h> according to the C standard.

--- src/drmrx/deinterleaver.cpp
+++ src/drmrx/deinterleaver.cpp
@@ -29,7 +29,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
 #include <math.h>
 int *deinterleaver(int xinA, int tA, int xinB, int tB)
 {
--- src/drmrx/getmode.cpp
+++ src/drmrx/getmode.cpp
@@ -34,7 +34,6 @@
 #include <stdlib.h>
 #include <math.h>
 #include <sys/types.h>
-#include <malloc.h>
 #include "structtemplates.h"
 #include "drmproto.h"
 #include "drmdefs.h"
