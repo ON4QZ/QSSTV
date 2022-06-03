6393dc12168d8049d5e7d1f89c339cabef096978
Joerg Wunsch <j@uriah.heep.sax.de>
2022-06-03 23:07:01 +0200

    In cameraDialog::getCameraInfo(), ensure list length > 0
    When getFormatList() returned nothing, there is no point in
    adding anything to the cameraList.

--- src/videocapt/cameradialog.cpp
+++ src/videocapt/cameradialog.cpp
@@ -220,7 +220,8 @@ void cameraDialog::getCameraInfo(QStringList devList)
       formats=getFormatList(fd);
       if(ok)
         {
-          cameraList.append(scameraDevice(camDev,(const char *)cap.card,(const char *)cap.driver,(const char *)cap.bus_info,formats));
+          if (formats.count() > 0)
+            cameraList.append(scameraDevice(camDev,(const char *)cap.card,(const char *)cap.driver,(const char *)cap.bus_info,formats));
         }
       v4l2_close(fd);
     }
