e6ca4478ccf8b4b01977ef5336f1196b39be6d9c
Joerg Wunsch <j@uriah.heep.sax.de>
2022-06-03 23:06:04 +0200

    Open the rig control port with O_NONBLOCK
    Required by FreeBSD, should not harm for other systems.

--- src/rig/rigcontrol.cpp
+++ src/rig/rigcontrol.cpp
@@ -387,7 +387,7 @@ void rigControl::activatePTT(bool b)
       if (catParams.pttSerialPort.isEmpty()) return;
       if(serialP==0)
         {
-          serialP=::open(catParams.pttSerialPort.toLatin1().data(),O_RDWR);
+          serialP=::open(catParams.pttSerialPort.toLatin1().data(),O_RDWR|O_NONBLOCK);
           if (serialP<=0)
             {
               QMessageBox::warning(txWidgetPtr,"Serial Port Error",
