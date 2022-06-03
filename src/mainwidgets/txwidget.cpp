37f7c2bf753a7d8fa025ea618921e3ace0a097ee
Joerg Wunsch <j@uriah.heep.sax.de>
2022-06-03 23:25:57 +0200

    Workaround for QSlider performance issue on MacOS 12.x
    By default, QSlider eats up a lot of CPU. The workaround is to
    apply a stylesheet, idea taken from a stackoverflow posting.
    Presumably fixed in Qt 6.3.

--- src/mainwidgets/txwidget.cpp
+++ src/mainwidgets/txwidget.cpp
@@ -91,6 +91,26 @@ txWidget::txWidget(QWidget *parent) :  QWidget(parent), ui(new Ui::txWidget)
   notifyTimer.setInterval(NOTIFYCHECKINTERVAL);
   repeaterTxDelayTimer.setSingleShot(true);
 
+#if defined(__APPLE__) && QT_VERSION < QT_VERSION_CHECK(6, 3, 0)
+  // workaround for performance issue on MacOS 12.x
+  // idea courtesy
+  // https://stackoverflow.com/questions/69890284/qslider-in-qt-misbehaves-in-new-macos-monterey-v12-0-1-any-workaround
+  ui->sizeSlider->setStyleSheet("\
+                      QSlider::groove:horizontal {\
+                          height: 8px; /* the groove expands to the size of the slider by default. by giving it a height, it has a fixed size */ \
+                          background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #B1B1B1, stop:1 #c4c4c4);\
+                          margin: 2px 0;\
+                      }\
+                      \
+                      QSlider::handle:horizontal {\
+                          background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #b4b4b4, stop:1 #8f8f8f);\
+                          border: 1px solid #5c5c5c;\
+                          width: 18px;\
+                          margin: -2px 0; /* handle is placed by default on the contents rect of the groove. Expand outside the groove */ \
+                          border-radius: 3px;\
+                      }\
+                  ");
+#endif  // __APPLE__ && Qt < 6.3
 }
 
 txWidget::~txWidget()
